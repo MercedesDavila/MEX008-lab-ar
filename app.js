196fe98cfb042a226bd3ab4a9c505f025bbc4155
MercedesDavila <mmdavila@outlook.com>
2019-07-21 23:24:41 -0500

    Se agregaron modales de Log In y Sign Up

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+//Working with login button
+const loginButton = document.getElementById('login-button');
+const closeButton = document.getElementById('close');
+const signupButton = document.getElementById('signup-button');
+const closesignup = document.getElementById('close-signup');
+
+
 //Function that hide section with id
 const hideSection = (id) => {
     document.getElementById(id).classList.add('hide');
@@ -6,3 +13,15 @@ const hideSection = (id) => {
 const showSection = (id) => {
     document.getElementById(id).classList.remove('hide');
 };
+
+//Functionability for help button and close button
+const showlogin = () => showSection('Login');
+const closelogin = () => hideSection('Login');
+const showsignup = () => showSection('signup');
+const closesp = () => hideSection('signup');
+
+
+loginButton.addEventListener("click", showlogin);
+closeButton.addEventListener("click", closelogin);
+signupButton.addEventListener("click", showsignup);
+closesignup.addEventListener("click", closesp);
